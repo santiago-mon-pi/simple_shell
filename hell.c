f86de1463c13d564ca274daf2a6645fa8c86328c
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-04-14 12:58:06 -0500

    added main function and header file

---
+++ hell.c
@@ -0,0 +1,17 @@
+#include "main.h"
+int main (void)
+
+{
+
+char *str = NULL;
+size_t line_size = 0;
+int cunt = 1;
+
+while (1)
+    {
+        puts("#placeholders");
+        getline(&str, &line_size, stdin);
+    }
+return(0);
+
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+#endif
