50f175f24fd6ea76435b3508996a16a4aa60017f
Mr-emilio <pablopeag@hotmail.com>
2022-04-23 20:29:34 -0500

    betty argc

--- argc.c
+++ argc.c
@@ -9,31 +9,23 @@
 
 int argc(char *str)
 {
-    char *tokenizer = NULL, *copy_str = NULL;
-    int len_str = 0;
-    
-    if (str == NULL)
-        perror("Error");
+	char *tokenizer = NULL, *copy_str = NULL;
+	int len_str = 0;
 
-   
-    len_str = _strlen(str);
-    copy_str = malloc(len_str + 1);
-
-    if (copy_str == NULL)
-        perror("Error");
-    
-   
-    copy_str = _strcpy(copy_str, str);
-
-    
-    tokenizer = strtok(copy_str, DELIM);
-    len_str = 0;
-
-    while (tokenizer != NULL)
-    {
-        len_str++;
-        tokenizer = strtok(NULL, DELIM);
-    }
-    free(copy_str);
-    return (len_str);
+	if (str == NULL)
+		perror("Error");
+	len_str = _strlen(str);
+	copy_str = malloc(len_str + 1);
+	if (copy_str == NULL)
+		perror("Error");
+	copy_str = _strcpy(copy_str, str);
+	tokenizer = strtok(copy_str, DELIM);
+	len_str = 0;
+	while (tokenizer != NULL)
+	{
+		len_str++;
+		tokenizer = strtok(NULL, DELIM);
+	}
+	free(copy_str);
+	return (len_str);
 }
