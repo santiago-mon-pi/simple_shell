89ac96e1ff8d0c8ec145b01628921c55257d06f8
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-04-23 21:02:13 -0500

    corrected code to be in line with betty style

--- path_extension.c
+++ path_extension.c
@@ -10,26 +10,27 @@
  */
 char *create_path_extension(char *cmd, char **argv, int argc)
 {
-    char *environment_path = NULL, *tokenzer = NULL, *token_buf = NULL;
 
-    if (cmd[0] != '/')
-    {
-        environment_path = get_path();
-        if (environment_path == NULL)
-        {
-            free_double_pointer(argv, argc);
-            perror("./shell");
-        }
-	tokenzer = get_route_bin(environment_path, tokenzer);
-        token_buf = validate_access(cmd, tokenzer, argv, argc);
-        if (token_buf == NULL)
-        {
-            free_double_pointer(argv, argc);
-            perror("./shell");
-        }
-        return token_buf;
-    }
-    return cmd;
+	char *environment_path = NULL, *tokenzer = NULL, *token_buf = NULL;
+
+	if (cmd[0] != '/')
+	{
+		environment_path = get_path();
+		if (environment_path == NULL)
+		{
+			free_double_pointer(argv, argc);
+			perror("./shell");
+		}
+		tokenzer = get_route_bin(environment_path, tokenzer);
+		token_buf = validate_access(cmd, tokenzer, argv, argc);
+		if (token_buf == NULL)
+		{
+			free_double_pointer(argv, argc);
+			perror("./shell");
+		}
+		return (token_buf);
+	}
+	return (cmd);
 }
 
 /**
@@ -39,28 +40,27 @@ char *create_path_extension(char *cmd, char **argv, int argc)
  */
 char *get_path()
 {
-    char *return_path = NULL;
-    int i, path_len = 0;
+	char *ret_path = NULL;
+	int i, path_len = 0;
 
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        if (_strcmp(environ[i], "PATH") == 0)
-        {
-            path_len = _strlen(environ[i]);
-            return_path = malloc(sizeof(char) * (path_len + 1));
-            if (return_path == NULL)
-            {
-                return NULL;
-            }
-            return_path = _strcpy(return_path, environ[i]);
-        }
-    }
-    return return_path;
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (_strcmp(environ[i], "PATH") == 0)
+		{
+			path_len = _strlen(environ[i]);
+			ret_path = malloc(sizeof(char) * (path_len + 1));
+			if (ret_path == NULL)
+			{
+				return (NULL);
+			}
+			ret_path = _strcpy(ret_path, environ[i]);
+		}
+	}
+	return (ret_path);
 }
 
-
 /**
- * split_path - Split the path and the token to keep only the path
+ * get_route_bin - Split the path and the token to keep only the path
  * @path: str to be split with strtok
  * @token: pointer to the str after =
  *
@@ -75,44 +75,46 @@ char *get_route_bin(char *path, char *token)
 }
 
 /**
- * check_access - check wheter the file exist or not in the path
- * @temp: malloc created to store the path to test with access
- * @token: path to be copy into temp
- * @command: command to be concatenated with the path
- * @path: path to be freed in sucess case
+ * validate_access - check wheter the file exist or not in the path
+ * @tokenizer: stores tokenization
+ * @cmd: command
+ * @argc: argument count
+ * @argv: argument variables
  *
  * Return: temp if access worked - NULL otherwise
  */
-char *validate_access(char *cmd, char*tokenizer ,char **argv, int argc)
+
+char *validate_access(char *cmd, char *tokenizer, char **argv, int argc)
+
 {
-    int tokenizer_len = 0;
-    char *return_token = NULL;
+	int tokenizer_len = 0;
+	char *ret_token = NULL;
 
-    while (tokenizer != NULL)
-    {
-        tokenizer_len = _strlen(tokenizer);
-        return_token = malloc(sizeof(char) * (tokenizer_len + _strlen(cmd) + 2));
-        if (return_token == NULL)
-        {
-            free_double_pointer(argv, argc);
-            return NULL;
-        }
+	while (tokenizer != NULL)
+	{
+		tokenizer_len = _strlen(tokenizer);
+		ret_token = malloc(sizeof(char) * (tokenizer_len + _strlen(cmd) + 2));
 
-        return_token = _strcpy(return_token, tokenizer);
-        return_token = _strcat(return_token, "/");
-        return_token = _strcat(return_token, cmd);
+		if (ret_token == NULL)
+		{
+			free_double_pointer(argv, argc);
+			return (NULL);
+		}
+		ret_token = _strcpy(ret_token, tokenizer);
+		ret_token = _strcat(ret_token, "/");
+		ret_token = _strcat(ret_token, cmd);
 
-        if (access(return_token, X_OK) == 0)
-        {
-            free(cmd);
-            return return_token;
-        }
-        else
-        {
-            return_token = cmd;
-        }
+		if (access(ret_token, X_OK) == 0)
+		{
+			free(cmd);
+			return (ret_token);
+		}
 
-        tokenizer = strtok(NULL, ":");
-    }
-    return return_token;
+		else
+		{
+			ret_token = cmd;
+		}
+		tokenizer = strtok(NULL, ":");
+	}
+	return (ret_token);
 }
