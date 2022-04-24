8566adb06882beabefd2d09543dab2cc57306a31
Mr-emilio <pablopeag@hotmail.com>
2022-04-23 20:59:56 -0500

    betty clean line

--- clean_line.c
+++ clean_line.c
@@ -1,20 +1,23 @@
 #include "main.h"
+/**
+ * clean_line - free the line
+ * @line: line to will free
+ * Return: line
+ */
 
 char *clean_line(char *line)
 {
-    int len_line = 0;
-    char *return_line = NULL;
+	int len_line = 0;
+	char *rturn_line = NULL;
 
-    
-    len_line = _strlen(line);
-    return_line = (char *)malloc(sizeof(char) * len_line);
-    if (return_line == NULL){
-        perror("Error");
-    }
-
-    return_line = _strcpy(return_line, line);
-    return_line[len_line - 1] = '\0';
-
-    return return_line;
+	len_line = _strlen(line);
+	rturn_line = (char *)malloc(sizeof(char) * len_line);
+	if (rturn_line == NULL)
+	{
+		perror("Error");
+	}
+	rturn_line = _strcpy(rturn_line, line);
+	rturn_line[len_line - 1] = '\0';
+	return (rturn_line);
 
 }
