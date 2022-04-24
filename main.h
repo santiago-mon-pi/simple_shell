b05ffb1129225f4d97de49d5ef9f2f3fa0d22753
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-04-23 19:56:32 -0500

    re-added main function and header

--- main.h
+++ main.h
@@ -1,33 +1,44 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _HEAD_H_
+#define _HEAD_H_
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
-#include <stddef.h>
+#include <signal.h>
 
+#define DELIM " \t\r\n\a"
+
+/* GET ENV */
 extern char **environ;
 
+/* FUNCTIONS STR */
 int _strlen(char *s);
+void print_line(char *str);
 int _strcmp(char *s1, char *s2);
-char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
-int _putchar(char c);
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
+void print_number(int n);
 void _puts(char *str);
-int _atoi(char *c);
-int arg_count(char *str);
-char **args_v(char *strings, int argc);
-void free_array(char **argv, int argc);
-void _exec(char **argv, char *buf, int argc, int count);
-char *path_validator(char *cmd, char **argv, int argc, char *tmp);
-char *path_tk(char *path, char *tk);
-char *path_acc(char *tk_buf, char *tk, char *cmd, char *path);
-void fork_process(char **argv, char *buf, int argc, int count);
-void system_error(char **argv, int count, char *error);
-void print_num(int n);
-char *path_ciclo();
+int _putchar(char c);
+
+
+/* ARGC | ARGV | FREE | EXEC | ERROR */
+void non_interactive(char *line);
+int argc(char *str);
+char **argv(char *line, int argument_count);
+char *clean_line(char *line);
+void free_double_pointer(char **argv, int argc);
+void *interactive(char *interactive_str, char *line);
+char *create_path_extension(char *cmd, char **argv, int argc);
+char *validate_access(char *cmd, char*tokenizer ,char **argv, int argc);
+char *get_route_bin(char *path, char *token);
+void run_command(char **argv_command, int argc, int bash_count);
+void create_process_to_execute(char **argv_command, int argc, int bash_count);
+char *get_path();
+void sys_error(char **argv, int count, char *error);
+
 
 #endif
